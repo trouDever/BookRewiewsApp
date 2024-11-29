544dc7e2c81b4df72cd46fe0ea41e1032a0d0502
trouDever <diana.gyulmagomedova@yandex.ru>
2024-11-30 01:24:21 +0300

    add the base of the project, buttons, and the first image. connected to firebase and configured storage with firestore database

---
+++ app/src/main/java/com/diana/bookstoreapp/data/Book.kt
@@ -0,0 +1,9 @@
+package com.diana.bookstoreapp.data
+
+data class Book(
+    val name: String = "",
+    val description: String = "",
+    val grade: String = "",
+    val category: String = "",
+    val imageUrl: String = ""
+)
